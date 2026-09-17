66ca1ad25669e2a59617d5d36d6078e0919dd6ca
kaungmyatkoko20001978-eng <kaungmyatkoko20001978@gmail.com>
2026-09-17 17:40:56 +0630

    Implement plagiarism checker function
    Added a plagiarism checking function using spaCy.

---
+++ Plagiarism_Checker_Version-2.py
@@ -0,0 +1,15 @@
+#You will need to install spacy first, type this into your terminal:
+#pip install spacy
+#I became a VERY VERY lazy programmer so...yeah...I just hopped onto the library 
+
+def check_plagiarism(filename1, filename2):
+    import spacy
+    with open(filename1, 'r') as f: #opening...reading files...just some boring stuff
+          text1 = f.read()
+    with open(filename2, 'r') as f:
+          text2 = f.read()
+    process = spacy.load("en_core_web_sm") #I loaded the language model here :D
+    s1=process(text1) #putting the file into the process object
+    s2=process(text2) #same here
+    percent=s1.similarity(s2)
+    print(f"Percentage: {percent:.2%}")
